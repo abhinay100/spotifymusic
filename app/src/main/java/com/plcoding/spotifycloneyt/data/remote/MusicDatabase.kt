e7df1026c64a0ea10d58ebf1c81b8ff91506a212
abhinay <abhinay@myhealthcare.life>
2024-02-01 23:31:06 +0530

    firebase setup

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/remote/MusicDatabase.kt
@@ -0,0 +1,23 @@
+package com.plcoding.spotifycloneyt.data.remote
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.plcoding.spotifycloneyt.data.entities.Song
+import com.plcoding.spotifycloneyt.other.Constants.SONG_COLLECTION
+import kotlinx.coroutines.tasks.await
+
+/**
+ * Created by Abhinay on 01/02/24.
+ */
+class MusicDatabase {
+
+    private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+}
---
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -0,0 +1,9 @@
+package com.plcoding.spotifycloneyt.other
+
+/**
+ * Created by Abhinay on 01/02/24.
+ */
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}
