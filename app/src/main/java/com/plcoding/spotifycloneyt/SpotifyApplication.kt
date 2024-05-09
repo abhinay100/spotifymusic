b077eeb40a2d98d6d583144e91a19945c0ac55b8
abhinay <abhinay@myhealthcare.life>
2024-05-09 17:23:56 +0530

    dagger setup

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/SpotifyApplication.kt
@@ -0,0 +1,10 @@
+package com.plcoding.spotifycloneyt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Abhinay on 02/04/24.
+ */
+@HiltAndroidApp
+class SpotifyApplication : Application()
